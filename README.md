b88bb8e2c0b7d21fd527a67b6c7fcfcfd9e832af
Sylvain UTARD <sylvain@sorare.com>
2021-12-17 22:42:14 +0100

    Bump to 1.0.0
     - Upgrade deps.
     - Cleanup legacy deps on react and gitlab
     - Reworked README

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# crypto
+<!-- logo -->
+<p align="center">
+  <img src="logo.png">
+</p>
 
-Crypto used in the Sorare stack
+<!-- tag line -->
+<h4 align='center'>JavaScript Crypto helpers for Sorare</h4>
+
+<!-- primary badges -->
+<p align="center">
+  <a href="https://www.typescriptlang.org/">
+    <img src='https://badges.aleen42.com/src/typescript.svg' />
+  </a> 
+  <a href="https://www.npmjs.com/package/@sorare/crypto">
+    <img src='https://img.shields.io/github/package-json/v/@sorare/crypto?label=npm' />
+  </a>
+  <a href="https://starkware.co/">
+    <img src="https://img.shields.io/badge/powered_by-StarkWare-navy">
+  </a>
+</p>
+
+`@sorare/crypto` is a JavaScript library providing various crypto functions to be used to sign your Sorare LimitOrder objects in order to make a bid, create or accept an offer. It can be used in both NodeJS and Browser environments.
+
+# Functions
+
+## `signLimitOrder`
+
+The `signLimitOrder(privateKey, limitOrder)` function is used to sign a `LimitOrder` object with a Sorare private key.
+
+```ts
+import { signLimitOrder } from '@sorare/crypto';
+
+const privateKey = /* Your Sorare private key */;
+const limitOrder = /* The LimitOrder object you get from GraphQL */;
+
+const signature = signLimitOrder(privateKey, limitOrder);
+```
+
+# License
+
+`@sorare/crypto` is [MIT licensed](LICENSE).
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { ec } from 'elliptic';
 import { hdkey } from 'ethereumjs-wallet';
 
 import { LimitOrder, Transfer, Signature } from './types';
-import { getAccountPath, getKeyPairFromPath } from './keyDerivation';
+import { getAccountPath, getKeyPairFromPath } from './starkware/keyDerivation';
 import {
   starkEc,
   sign as starkSign,
@@ -13,9 +13,9 @@ import {
   getTransferMsgHashWithFee,
   getLimitOrderMsgHash,
   getLimitOrderMsgHashWithFee,
-} from './signature';
+} from './starkware/signature';
 
-export const PATH = "m/44'/60'/0'/0/0";
+const PATH = "m/44'/60'/0'/0/0";
 
 export const generateKey = (mnemonic?: string) => {
   const seed = mnemonicToSeedSync(mnemonic || generateMnemonic());
@@ -29,26 +29,22 @@ export const generateKey = (mnemonic?: string) => {
   return getKeyPairFromPath(mnemonic, path);
 };
 
-export const exportPrivateKey = (key: ec.KeyPair) =>
+export const exportPrivateKey = (key: ec.KeyPair): string =>
   `0x${key.getPrivate('hex').padStart(64, '0')}`;
 
-export const exportPublicKey = (key: ec.KeyPair) =>
+export const exportPublicKey = (key: ec.KeyPair): string =>
   `0x${key.getPublic(true, 'hex')}`;
 
-export const exportPublicKeyX = (key: ec.KeyPair) =>
-  `0x${key
-    .getPublic()
-    .getX()
-    .toString('hex')
-    .padStart(64, '0')}`;
+export const exportPublicKeyX = (key: ec.KeyPair): string =>
+  `0x${key.getPublic().getX().toString('hex').padStart(64, '0')}`;
 
-export const loadPrivateKey = (privateKey: string) =>
+export const loadPrivateKey = (privateKey: string): ec.KeyPair =>
   starkEc.keyFromPrivate(privateKey.substring(2), 'hex');
 
-export const loadPublicKey = (publicKey: string) =>
+export const loadPublicKey = (publicKey: string): ec.KeyPair =>
   starkEc.keyFromPublic(publicKey.substring(2), 'hex');
 
-export const hashTransfer = (transfer: Transfer) => {
+const hashTransfer = (transfer: Transfer) => {
   const {
     amount,
     nonce,
@@ -83,7 +79,7 @@ export const hashTransfer = (transfer: Transfer) => {
   return getTransferMsgHash(...args);
 };
 
-export const hashLimitOrder = (limitOrder: LimitOrder) => {
+const hashLimitOrder = (limitOrder: LimitOrder) => {
   const {
     vaultIdSell,
     vaultIdBuy,
@@ -118,7 +114,7 @@ export const hashLimitOrder = (limitOrder: LimitOrder) => {
   return getLimitOrderMsgHash(...args);
 };
 
-export const sign = (privateKey: string, message: string) => {
+const sign = (privateKey: string, message: string) => {
   const key = loadPrivateKey(privateKey);
   const { r, s } = starkSign(key, message);
 
@@ -128,11 +124,7 @@ export const sign = (privateKey: string, message: string) => {
   };
 };
 
-export const verify = (
-  publicKey: string,
-  message: string,
-  signature: Signature
-) => {
+const verify = (publicKey: string, message: string, signature: Signature) => {
   const key = loadPublicKey(publicKey);
   const sig = {
     r: new BN(signature.r.substring(2), 16),
@@ -142,7 +134,10 @@ export const verify = (
   return starkVerify(key, message, sig);
 };
 
-export const signTransfer = (privateKey: string, transfer: Transfer) => {
+export const signTransfer = (
+  privateKey: string,
+  transfer: Transfer
+): Signature => {
   const message = hashTransfer(transfer);
 
   return sign(privateKey, message);
@@ -152,13 +147,16 @@ export const verifyTransfer = (
   publicKey: string,
   transfer: Transfer,
   signature: Signature
-) => {
+): boolean => {
   const message = hashTransfer(transfer);
 
   return verify(publicKey, message, signature);
 };
 
-export const signLimitOrder = (privateKey: string, limitOrder: LimitOrder) => {
+export const signLimitOrder = (
+  privateKey: string,
+  limitOrder: LimitOrder
+): Signature => {
   const message = hashLimitOrder(limitOrder);
 
   return sign(privateKey, message);
@@ -168,7 +166,7 @@ export const verifyLimitOrder = (
   publicKey: string,
   limitOrder: LimitOrder,
   signature: Signature
-) => {
+): boolean => {
   const message = hashLimitOrder(limitOrder);
 
   return verify(publicKey, message, signature);
