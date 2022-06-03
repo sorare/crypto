3ea33332fa053834207928dcb9ea18e8fc7ececd
Sylvain UTARD <sylvain.utard@gmail.com>
2022-06-03 15:49:37 +0200

    WIP

--- src/crypto-cpp/src/starkware/crypto/ffi/utils.cc
+++ src/crypto-cpp/src/starkware/crypto/ffi/utils.cc
@@ -1,4 +1,11 @@
+#ifdef __APPLE__
+#include <machine/endian.h>
+#include <libkern/OSByteOrder.h>
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
+#else
 #include <endian.h>
+#endif
 #include <algorithm>
 #include <cstring>
 
